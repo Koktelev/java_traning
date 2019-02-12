44e589803e2a2a0a187549c77ad1ce52d6beac86
Koktelev <47237772+Koktelev@users.noreply.github.com>
2019-02-12 20:07:08 +0300

    Point доработки

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,19 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-public class Point { double x , y;
-    public static void main (String[] args){
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x = 10;
-        p2.x = 20;
-        p1.y = 10;
-        p2.y = 20;
+public class Point {
+    double x , y;
 
-
-
-        System.out.println( "Расстояние между точками = " + distance(p1,p2));
-
-    }
     public static double distance(Point p1,Point p2) {
 
         return Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
