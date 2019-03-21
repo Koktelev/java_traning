a311f000c03c42b41ccca8410027f91a0c78fc52
Koktelev <47237772+Koktelev@users.noreply.github.com>
2019-03-21 10:15:32 +0300

    Browser

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,20 +2,37 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     protected WebDriver wd;
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
     private  ContactHelper contactHelper ;
     private SessionHelper sessionHelper;
     private  NavigationHelper navigationHelper ;
     private  GroupHelper groupHelper ;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
     protected void init() {
-       wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         contactHelper = new ContactHelper(wd);
